b07240ad55ac05ad99c85ccbccfb4eb081ba2302
Justin Miller <indymiller@gmail.com>
2020-06-19 17:49:45 -0400

    added login security

--- bugtracker/urls.py
+++ bugtracker/urls.py
@@ -3,4 +3,7 @@ from bugtracker import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.loginview, name='login'),
+    path('logout/', views.logoutview, name='logout'),
+    
 ]
--- bugtracker/views.py
+++ bugtracker/views.py
@@ -1,11 +1,40 @@
 from django.shortcuts import render
-from bugtracker.forms import AddTicketForm
-from bugtracker.models import Ticket
+from django.contrib import messages
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect, reverse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+
+from bugtracker.forms import AddTicketForm, LoginForm
+from bugtracker.models import Ticket, MyUser
 
 # Create your views here.
+@login_required
 def home(request):
     return render(request, 'home.html',
                   {'tickets': Ticket.objects.all()})
+
+def loginview(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(
+                request, username=data['username'], password=data['password']
+                )
+            if user:
+                login(request, user)
+                return HttpResponseRedirect(
+                    request.GET.get('next', reverse('homepage'))
+                )
+    form = LoginForm()
+    return render(request, 'generic_form.html', {'form': form})
+
+def logoutview(request):
+    logout(request)
+    messages.info(request, "Logged out successfully!")
+    return HttpResponseRedirect(
+                    request.GET.get('next', reverse('index'))
+                )
 
 
 def add_ticket(request):
